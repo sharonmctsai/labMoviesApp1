a25cdd0cc36cbf31ba3b826fd1b49228efd47dfa
Sharon Tsai <sharonmctsai@gmail.com>
2024-07-06 12:10:15 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import MovieDetailsPage from './pages/movieDetailsPage'
+import {MovieImage} from './types/interfaces'
+import sample from './stories/sampleData'
+
+const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const App = () => {
+    return (
+      <MovieDetailsPage movie={sample} images={images} />
+    );
+  };
+
+const rootElement = createRoot(document.getElementById("root")!); 
+rootElement.render(<App />);
+
+const images: MovieImage[] = [
+    { file_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg"},
+    { file_path: "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg"},
+    { file_path: "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg"},
+    { file_path: "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg"},
+  ];
---
+++ src/types/interfaces.ts
@@ -0,0 +1,39 @@
+export interface BaseMovieProps {
+    title: string;
+    budget: number;
+    homepage: string | undefined;
+    id: number;
+    imdb_id: string;
+    original_language: string;
+    overview: string;
+    release_date: string;
+    vote_average: number;
+    popularity: number;
+    poster_path?: string;
+    tagline: string;
+    runtime: number;
+    revenue: number;
+    vote_count: number;
+    favourite?: boolean;
+    movies: BaseMovie[];
+    genres: {
+        id: number;
+        name: string;
+      }[];
+
+  }
+
+  export interface MovieImage {
+    file_path: string;
+    aspect_ratio?: number; //some props are optional...
+    height?: number;
+    iso_639_1?: string;
+    vote_average?: number;
+    vote_count?: number;
+    width?: number;
+  }
+  
+  export interface MoviePageProps {
+    movie: MovieDetailsProps;
+    images: MovieImage[];
+  }
